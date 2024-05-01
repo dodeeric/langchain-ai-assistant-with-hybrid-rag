69239213e8506d80841ac51890e7a20393f264a3
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-01 13:00:34 +0200

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -67,7 +67,7 @@ st.markdown("- Pouvez-vous me montrer un tableau de Jan Verhas provenant de la b
 st.markdown("- Qui est présent sur le tableau 'la revue des écoles' ?")
 st.markdown("- Pouvez-vous me montrer des images sur lesquelles ce trouve la reine Marie-Henriette ? Pouvez-vous me donner les auteurs des images ?")
 st.markdown("- Pouvez-vous me montrer un portrait du roi Léopol Ier ? Il faut que ce soit une gravure.")
-st.markdwon("Si vous n'obtenez pas une réponse correcte, essayez de reformuler la question. Par exemple la question suivante ne reçois pas de réponse correcte : *Avez-vous un buste de Louis-Philipe, fils du roi Léopold Ier ?*, mais la question suivante reçoit elle une réponse correcte : *Avez-vous un buste de Louis-Philipe ?*.
+st.markdown("Si vous n'obtenez pas une réponse correcte, essayez de reformuler la question. Par exemple la question suivante ne reçois pas de réponse correcte : *Avez-vous un buste de Louis-Philipe, fils du roi Léopold Ier ?*, mais la question suivante reçoit elle une réponse correcte : *Avez-vous un buste de Louis-Philipe ?*.
 st.markdown("L'assistant prend environ 30 secondes pour répondre.")
 st.markdown("Pour l'instant, l'assistant ne possède pas de mémoire de la session de questions et réponses. Les questions que vous posez ne peuvent donc pas faire référence aux questions et réponses précédentes.")
 st.markdown(" ")
