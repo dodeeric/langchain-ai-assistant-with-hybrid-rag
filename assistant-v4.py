3df2afe6d96423112ef9f255dcc4b134681502f2
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-05 16:11:45 +0200

    Update assistant-v4.py

--- assistant-v4.py
+++ assistant-v4.py
@@ -195,12 +195,32 @@ with st.sidebar:
 if 'chat_history' not in st.session_state: # Mandatory
     st.session_state.chat_history = []
 
-with st.chat_message("assistant"):
-    st.write("Bonjour 👋")
+# -----------------------------------------------
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+# -----------------------------------------------
 
 question = st.text_area("Entrez votre question :", help='Type your question here and press Control-Enter.')
 
