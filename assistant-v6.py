63d22e575e6be32a4ac1891700c3c49801de8d66
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-08 12:15:48 +0200

    Update assistant-v6.py

--- assistant-v6.py
+++ assistant-v6.py
@@ -96,8 +96,9 @@ def instanciate_retrievers_and_chains(_documents, _vector_db):
     You first have to search answers in the "Knowledge Base". If no answers are found in the "Knowledge Base", then answer with your own knowledge. \
     You have to answer in the same language as the question.
     At the end of the answer:
-    - at a new line, display an image of the artwork (see the "og:image" field).
-    - at a new line, write "Reference: " (in the language of the question) followed by the link to the web page about the artwork (see the "url" field).
+    - At a new line, display an image of the artwork (see the "og:image" field).
+    - At a new line, write "Reference: " (in the language of the question) followed by the link to the web page about the artwork (see the "url" field). \
+    For Wikimedia Commons, the text of the link has to be the title of the web page (see "og:title").
 
     Knowledge Base:
 
