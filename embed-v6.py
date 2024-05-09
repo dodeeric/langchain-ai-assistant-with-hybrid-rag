20d8bae028b7f76c6befef2b2d020f391cd2bf6e
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-09 20:37:38 +0200

    Update embed-v6.py

--- embed-v6.py
+++ embed-v6.py
@@ -125,7 +125,7 @@ for xml_file in xml_files:
     #print(f">>> xml_file: {xml_file}")
     xml_path = f"/root/download.europeana.eu/dataset/XML/{xml_file}"
     print(f">>> xml_path: {xml_path}")
-    if i < 25:
+    if i < 1000:
         xml_paths.append(xml_path)
 
 documents = load_files(paths, pdf_paths, xml_paths)
