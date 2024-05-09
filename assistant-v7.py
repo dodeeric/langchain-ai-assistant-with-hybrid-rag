2acbc590084aa083c2e2f17b90703206017771a8
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-09 11:22:56 +0200

    Update assistant-v7.py

--- assistant-v7.py
+++ assistant-v7.py
@@ -176,7 +176,8 @@ with st.sidebar:
     ### About this assistant
     
     This AI (Artificial Intelligence) assistant allows you to ask all kinds of questions regarding art and the Belgian monarchy. To answer, the assistant \
-    queries the pictures databases BALaT of the IRPA (Royal Institute of Artistic Heritage), Belgica of the KBR (Royal Library) and Wikimedia Commons.
+    queries the images databases BALaT of the IRPA (Royal Institute of Artistic Heritage), Belgica of the KBR (Royal Library) and Wikimedia Commons. At the \
+    moment, about 2000 images are available.
 
     The questions can be in any language, but French and Dutch give the best results. If you don't get a correct answer, try rephrasing the question. The \
     assistant takes about 30 seconds to respond. He has a memory of the questions and answers session. The questions you ask may therefore \
@@ -188,7 +189,8 @@ with st.sidebar:
     ### Concernant cet assistant
     
     Cet assistant IA (Intelligence Artificielle) vous permet de poser toutes sortes de questions concernant l'art et la monarchie belge. Pour répondre, l'assistant \
-    questionne les bases de données d'images BALaT de l'IRPA (Institut royal du Patrimoine artistique), Belgica de la KBR (Bibliothèque royale) et Wikimedia Commons.
+    questionne les bases de données d'images BALaT de l'IRPA (Institut royal du Patrimoine artistique), Belgica de la KBR (Bibliothèque royale) et Wikimedia Commons. \
+    Pour l'instant, environ 2000 images sont disponible.
 
     Les questions peuvent-être posées en diférentes langues, mais le français et le néerlandais donnent les meilleurs résultats. Si vous n'obtenez pas une réponse \
     correcte, essayez de reformuler la question. L'assistant prend environ 30 secondes pour répondre. Il possède une mémoire de la session de questions et réponses. \
