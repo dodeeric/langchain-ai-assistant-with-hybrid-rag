5d215fbc05dfc9c784efeeffbf850de5317f8f68
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-10 14:36:45 +0200

    Update embed-xml-v1.py

--- embed-xml-v1.py
+++ embed-xml-v1.py
@@ -27,36 +27,6 @@ logging.basicConfig(filename='error.log', level=logging.ERROR)
 
 dotenv.load_dotenv()
 
-def load_files_and_embed(json_file_paths, pdf_file_paths):
-    # Loads and chunks files into a list of documents then embed
-
-    EMBEDDING_MODEL = "text-embedding-3-large"
-    COLLECTION_NAME = "bmae"
-    embedding_model = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-    
-    nbr_files = len(json_file_paths)
-    print(f">>> Embed {nbr_files} JSON files...")
-    documents = []
-    for json_file_path in json_file_paths:
-        print(f">>> JSON file: {json_file_path}")
-        loader = JSONLoader(file_path=json_file_path, jq_schema=".[]", text_content=False)
-        docs = loader.load()   # 1 JSON item per chunk
-        documents = documents + docs
-    Chroma.from_documents(documents, embedding_model, collection_name=COLLECTION_NAME, persist_directory="./chromadb")
-
-    nbr_files = len(pdf_file_paths)
-    print(f">>> Embed {nbr_files} PDF files...")
-    documents = []
-    if pdf_file_paths:   # if equals to "", then skip
-        for pdf_file_path in pdf_file_paths:
-            print(f">>> PDF file: {pdf_file_path}")
-            loader = PyPDFLoader(pdf_file_path)
-            pages = loader.load_and_split() # 1 pdf page per chunk
-            documents = documents + pages
-    Chroma.from_documents(documents, embedding_model, collection_name=COLLECTION_NAME, persist_directory="./chromadb")
-
-    return "JSON/PDF file done"
-
 def load_files_and_embed_xml():
     # Loads and chunks files into a list of documents then embed
     # Valid only for RDF/XML from Europeana for IRPA/BALaT
@@ -132,19 +102,4 @@ def load_files_and_embed_xml():
 
 # Load and index
 
-# JSON files
-files = os.listdir("./files/")
-paths = []
-for file in files:
-    path = f"./files/{file}"
-    paths.append(path)
-
-# PDF files
-pdf_files = os.listdir("./pdf_files/")
-pdf_paths = []
-for pdf_file in pdf_files:
-    pdf_path = f"./pdf_files/{pdf_file}"
-    pdf_paths.append(pdf_path)
-
-load_files_and_embed(paths, pdf_paths)
 load_files_and_embed_xml()
