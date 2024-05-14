3c7f2a8494242a54f6f516f05ded26f748029a65
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-14 14:37:35 +0200

    Update assistant-v9.py

--- assistant-v9.py
+++ assistant-v9.py
@@ -125,7 +125,7 @@ def instanciate_retrievers_and_chains(_vector_db, model):
         You first have to search answers in the "Knowledge Base". If no answers are found in the "Knowledge Base", then answer with your own knowledge. \
         You have to answer in the same language as the question.
         At the end of the answer:
-        - At a new line, display an image of the artwork (see the "og:image" field). Do not display an image which has been displayed already (see "Chat History").
+        - At a new line, display an image of the artwork (see the "og:image" field). Do not display any images if an image of the artwork has been displayed already (see "Chat History").
         - At a new line, write "More information: " (in the language of the question) followed by the link to the web page about the artwork (see the "url" field). \
         For Wikimedia Commons, the text of the link has to be the title of the web page WITHOUT the word "File" at the beginning (see "og:title").
     
