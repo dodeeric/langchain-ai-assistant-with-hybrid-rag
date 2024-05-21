356efe36934a290137ecf8fba2199f380207ef5a
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-21 16:52:57 +0200

    Update assistant_frontend_v4.py: typo

--- modules/assistant_frontend_v4.py
+++ modules/assistant_frontend_v4.py
@@ -159,7 +159,7 @@ def assistant_frontend():
     All related to Streamlit and connection with the Langchain backend. Includes also the admin interface.
     """
 
-    st.set_page_config(page_titel="Belgian Monarchy Artworks Explorer", page_icon="👑")
+    st.set_page_config(page_title="Belgian Monarchy Artworks Explorer", page_icon="👑")
     
     # Initialize chat history (chat_history) for LangChain
     if 'chat_history' not in st.session_state:
