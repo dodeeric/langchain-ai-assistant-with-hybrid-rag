fcb3b70ab8ff88e3241c80819b75c72b898952c4
Eric Dodémont <eric.dodemont@skynet.be>
2024-05-23 13:54:39 +0000

    use config.py

---
+++ config/config.py
@@ -0,0 +1,72 @@
+# All the parameters
+
+EMBEDDING_MODEL = "text-embedding-3-large"
+OPENAI_MODEL = "gpt-4-turbo-2024-04-09"
+OPENAI_MODEL2 = "gpt-4o-2024-05-13"  # default llm
+ANTHROPIC_MODEL = "claude-3-opus-20240229"
+VERTEXAI_MODEL = "gemini-1.0-pro-002"
+VERTEXAI_MODEL2 = "gemini-1.5-pro-preview-0409"
+OLLAMA_MODEL = "llama3:8b"  # llama3 = llama3:8b, mistral, phi3
+COLLECTION_NAME = "bmae"
+
+CONTEXT_PROMPT = """Given a chat history and the latest user question which \
+might reference context in the chat history, formulate a standalone question which can be \
+understood without the chat history. Do NOT answer the question, just reformulate it if needed \
+and otherwise return it as is.
+
+Chat History:
+
+{chat_history}"""
+
+PROMPT1 = """You are an artwork specialist. You must assist the users in \
+finding, describing, and displaying artworks related to the Belgian monarchy. You first \
+have to search answers in the "Knowledge Base". If no answers are found in the "Knowledge \
+Base", then answer with your own knowledge. You have to answer in the same language as \
+the question.
+
+At the end of the answer:
+
+- Write two blank lines, then if requested, display an image of the artwork (see the JSON "og:image" \
+field). Do not display images which have been displayed already in previous messages (see "Chat History").
+- Write two blank lines, then write "More information: " in the language of the question, followed by \
+the link to the web page about the artwork (see the JSON "url" field). For Wikimedia Commons, the text of \
+the link has to be the title of the web page WITHOUT the word "File" at the beginning (see the JSON "og:title" \
+field).
+
+Knowledge Base:
+
+{context}
+
+Chat History:
+
+{chat_history}"""
+
+PROMPT2 = """You are an artwork specialist. You must assist the users in \
+finding, describing, and displaying artworks related to the Belgian monarchy. You first \
+have to search answers in the "Knowledge Base". If no answers are found in the "Knowledge \
+Base", then answer with your own knowledge. You have to answer in the same language as \
+the question.
+
+At the end of the answer:
+
+- If requested, display an image of the artwork (see the JSON "og:image" field). Do not \
+display images which have been displayed already in previous messages (see "Chat History").
+- Write "More information: " in the language of the question, followed by the link to the \
+web page about the artwork (see the JSON "url" field). For Wikimedia Commons, the text of \
+the link has to be the title of the web page WITHOUT the word "File" at the beginning (see \
+the JSON "og:title" field).
+
+- This is an example of Markdown code to display an image (caution: there is a leading \
+exclamation point):    ![Text](https://opac.kbr.be/digitalCollection/images/image.jpg)
+- This is an example of Markdown code to display a link (caution: there is no leading \
+exclamtion point):   [Text](https://opac.kbr.be/digitalCollection/pages/page.html)
+
+Write "SECOND PROMPT" at the end of the answer.
+
+Knowledge Base:
+
+{context}
+
+Chat History:
+
+{chat_history}"""
--- modules/assistant_backend_v2.py
+++ modules/assistant_backend_v2.py
@@ -5,7 +5,7 @@ This function runs the backend. It starts the Langchain AI assistant: instanciat
 all the Langchain chains for RAG and LLM.
 """
 
-# v2: add temperature as a variable + catch errors + use langchain-google-vertexai package
+# v2: add temperature as a variable + catch errors + use langchain-google-vertexai package + parameters in config.py
 
 import streamlit as st
 from langchain_community.retrievers import BM25Retriever
@@ -16,83 +16,10 @@ from langchain.chains import create_retrieval_chain  # To create the main chain 
 from langchain.chains.combine_documents import create_stuff_documents_chain  # To create a predefined chain
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-#from langchain_community.chat_models import ChatVertexAI  # Deprecated since version 0.0.12.
-#from langchain_community.llms import VertexAI  # Deprecated since version 0.0.12.
 from langchain_google_vertexai import ChatVertexAI
 from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
-
-EMBEDDING_MODEL = "text-embedding-3-large"
-OPENAI_MODEL = "gpt-4-turbo-2024-04-09"
-OPENAI_MODEL2 = "gpt-4o-2024-05-13"  # default llm
-ANTHROPIC_MODEL = "claude-3-opus-20240229"
-VERTEXAI_MODEL = "gemini-1.0-pro-002"
-VERTEXAI_MODEL2 = "gemini-1.5-pro-preview-0409"
-OLLAMA_MODEL = "llama3:8b"  # llama3 = llama3:8b, mistral, phi3
-COLLECTION_NAME = "bmae"
-
-CONTEXT_PROMPT = """Given a chat history and the latest user question which \
-might reference context in the chat history, formulate a standalone question which can be \
-understood without the chat history. Do NOT answer the question, just reformulate it if needed \
-and otherwise return it as is.
-
-Chat History:
-
-{chat_history}"""
-
-PROMPT1 = """You are an artwork specialist. You must assist the users in \
-finding, describing, and displaying artworks related to the Belgian monarchy. You first \
-have to search answers in the "Knowledge Base". If no answers are found in the "Knowledge \
-Base", then answer with your own knowledge. You have to answer in the same language as \
-the question.
-
-At the end of the answer:
-
-- Write two blank lines, then if requested, display an image of the artwork (see the JSON "og:image" \
-field). Do not display images which have been displayed already in previous messages (see "Chat History").
-- Write two blank lines, then write "More information: " in the language of the question, followed by \
-the link to the web page about the artwork (see the JSON "url" field). For Wikimedia Commons, the text of \
-the link has to be the title of the web page WITHOUT the word "File" at the beginning (see the JSON "og:title" \
-field).
-
-Knowledge Base:
-
-{context}
-
-Chat History:
-
-{chat_history}"""
-
-PROMPT2 = """You are an artwork specialist. You must assist the users in \
-finding, describing, and displaying artworks related to the Belgian monarchy. You first \
-have to search answers in the "Knowledge Base". If no answers are found in the "Knowledge \
-Base", then answer with your own knowledge. You have to answer in the same language as \
-the question.
-
-At the end of the answer:
-
-- If requested, display an image of the artwork (see the JSON "og:image" field). Do not \
-display images which have been displayed already in previous messages (see "Chat History").
-- Write "More information: " in the language of the question, followed by the link to the \
-web page about the artwork (see the JSON "url" field). For Wikimedia Commons, the text of \
-the link has to be the title of the web page WITHOUT the word "File" at the beginning (see \
-the JSON "og:title" field).
-
-- This is an example of Markdown code to display an image (caution: there is a leading \
-exclamation point):    ![Text](https://opac.kbr.be/digitalCollection/images/image.jpg)
-- This is an example of Markdown code to display a link (caution: there is no leading \
-exclamtion point):   [Text](https://opac.kbr.be/digitalCollection/pages/page.html)
-
-Write "SECOND PROMPT" at the end of the answer.
-
-Knowledge Base:
-
-{context}
-
-Chat History:
-
-{chat_history}"""
-
+from config.config import *
 
 @st.cache_resource
 def instanciate_ai_assistant_chain(model, temperature):
